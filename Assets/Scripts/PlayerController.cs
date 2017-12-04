0c56f0272e7d8515400b1ad18178d8edf1ba26e0
Hallur A. Atlason <halluratlason@gmail.com>
2017-12-04 13:48:29 +0000

    Basic player movement (#7)
    * Added player sprite
    
    * Added player prefab
    
    * Implemented basic movement (note: no rotation)
    
    * Removed gravity and added Rigidbody2D component to player prefab
    
    * Set tag and reset transform

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+
+	public void Update()
+	{
+        // Get input
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        // TODO: Rotate object
+
+        // Move object
+        float x = transform.position.x + (moveX * speed * Time.deltaTime);
+        float y = transform.position.y + (moveY * speed * Time.deltaTime);
+
+        transform.position = new Vector2(x, y);
+	}
+}
