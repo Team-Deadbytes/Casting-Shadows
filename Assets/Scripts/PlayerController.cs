a2c686ca5cf3a9681b666c5790e5552d76f3dcc9
Hallur A. Atlason <halluratlason@gmail.com>
2017-12-04 15:30:40 +0000

    Implemented another movement method (#11)
    * Implemented another movement method
    
    * Set default rotational speed to 3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
         public float speed;
+        public float rotationalSpeed;
 
         private Animator animator;
 
@@ -14,6 +15,11 @@ public class PlayerController : MonoBehaviour
         }
 
         public void Update()
+        {
+                Move2();
+        }
+
+        private void Move1()
         {
                 // Get input
                 float moveX = Input.GetAxis("Horizontal");
@@ -32,5 +38,34 @@ public class PlayerController : MonoBehaviour
                         animator.SetTrigger("playerWalkEnd");
 
                 transform.position = new Vector2(x, y);
+        }
+
+        private void Move2()
+        {
+                // Rotate
+                if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+                {
+                        float angle = 0.0f;
+                        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+                                angle = rotationalSpeed;
+                        else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+                                angle = -1 * rotationalSpeed;
+                        transform.Rotate(0.0f, 0.0f, angle);
+                }
+
+                // Move
+		if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+                {
+                        // Start player-walk animation
+                        animator.SetTrigger("playerWalkStart");
+
+                        if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+                                transform.position += transform.right * speed * Time.deltaTime;
+                        else if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W))
+                                transform.position += (-1 * transform.right) * (speed / 2) * Time.deltaTime;
+                }
+		else
+                        // Stop player-walk animation
+			animator.SetTrigger("playerWalkEnd");
         }
 }
