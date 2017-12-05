5166848aec13e05637f19372ce0c84d3948b36af
Daði Sigursveinn Harðarson <dadi.sigursveinn@gmail.com>
2017-12-05 10:30:41 +0000

    added lightbubls that player can toggle with E in close proximity (#13)
    * added lightbubls that player can toggle with E in close proximity
    
    * removed initial value of lightOn in lightbubl controller
    
    * Added Objects tag and tagged lightBulb

---
+++ Assets/Scripts/LightBulbController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightBulbController : MonoBehaviour
+{
+
+    public bool lightOn;
+    public float detectionRange;
+    public GameObject player;
+    public SpriteRenderer sr;
+    public Sprite lightBulbOn, lightBulbOff;
+    bool closeEnough;
+    void Update()
+    {
+        closeEnough = false;
+        if (Vector3.Distance(player.transform.position, transform.position) <= detectionRange)
+            closeEnough = true;
+        if (Input.GetKeyUp(KeyCode.E) && closeEnough)
+            toggleLight();
+    }
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        sr.sprite = lightOn ? lightBulbOn : lightBulbOff;
+    }
+
+    private void toggleLight()
+    {
+        lightOn = !lightOn;
+        sr.sprite = lightOn ? lightBulbOn : lightBulbOff;
+    }
+}
