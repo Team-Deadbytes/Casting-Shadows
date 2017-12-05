61b07274b07b4c803af54bc0ac3cac7e0e846829
Hallur A. Atlason <halluratlason@gmail.com>
2017-12-05 17:42:46 +0000

    Implemented so that a message is displayed when the player is next to a door

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class DoorController : MonoBehaviour
 {
-	public bool isOpen;
+	private const string CLOSE_MESSAGE = "Press E to close door";
+	private const string OPEN_MESSAGE  = "Press E to open door";
+
+	private string proximityMessage = OPEN_MESSAGE;
+	private bool showProximityMessage;
+	private bool isOpen;
+
+	public void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+			showProximityMessage = true;
+	}
 
 	public void OnTriggerStay2D(Collider2D other)
 	{
@@ -13,10 +24,34 @@ public class DoorController : MonoBehaviour
 				toggleDoor();
 	}
 
+	public void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+			showProximityMessage = false;
+	}
+
 	private void toggleDoor()
 	{
 		float rotationAngle = isOpen ? -90.0f : 90.0f;
 		transform.Rotate(0.0f, 0.0f, rotationAngle);
 		isOpen = !isOpen;
+		proximityMessage = !isOpen ? OPEN_MESSAGE : CLOSE_MESSAGE;
+	}
+
+	private void OnGUI()
+	{
+		if (showProximityMessage)
+		{
+			GUIStyle labelStyle = GUI.skin.GetStyle("Label");
+			labelStyle.alignment = TextAnchor.UpperCenter;
+
+			float labelWidth = 300f;
+			float labelHeight = 50f;
+			float labelX = (Screen.width - labelWidth) / 2;
+			float labelY = Screen.height - 100;
+			Rect labelRect = new Rect(labelX, labelY, labelWidth, labelHeight);
+
+			GUI.Label(labelRect, proximityMessage, labelStyle);
+		}
 	}
 }
