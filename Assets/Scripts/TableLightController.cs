c037c92469e69d09accc9c3b0cff2c1590b52616
Njall Hilmar Hilmarsson <njall13@ru.is>
2017-12-05 19:40:51 +0000

    Added prefabs for other carpet sprites, table and table light and some coding for them

---
+++ Assets/Scripts/TableLightController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableLightController : MonoBehaviour {
+    private SpriteRenderer renderer;
+    public Sprite[] sprites;
+
+    [SerializeField]
+    bool state;
+
+	// Use this for initialization
+	void Start () {
+        renderer = gameObject.GetComponent<SpriteRenderer>();
+        if (state)
+        {
+            renderer.sprite = sprites[state ? 1 : 0];
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
