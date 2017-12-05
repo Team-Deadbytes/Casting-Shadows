fc730a892dfbec44fc47e67544b4fb407211782b
Hallur A. Atlason <halluratlason@gmail.com>
2017-12-05 22:09:01 +0000

    Made a camera script that makes the camera follow the player without rotating (#29)

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject player;
+
+	private Vector3 offset;
+
+	public void Start()
+	{
+		offset = transform.position - player.transform.position;
+	}
+
+	public void LateUpdate()
+	{
+		transform.position = player.transform.position + offset;
+	}
+}
