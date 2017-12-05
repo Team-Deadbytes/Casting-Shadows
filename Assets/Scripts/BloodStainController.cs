42036794b3c13f6915e98b60cfcf9ad0ee5baa56
Njall Hilmar Hilmarsson <njall13@ru.is>
2017-12-05 19:11:14 +0000

    Added bloodstain prefab, carpet prefab and added floors to its own category

---
+++ Assets/Scripts/BloodStainController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Random = UnityEngine.Random;
+
+public class BloodStainController : MonoBehaviour {
+    private SpriteRenderer renderer;
+
+    [SerializeField]
+    public Sprite[] sprites;
+    [SerializeField]
+    public bool keepDefault;
+
+    void Awaken()
+    {
+        keepDefault = false;
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (!keepDefault)
+        {
+            renderer = gameObject.GetComponent<SpriteRenderer>();
+            renderer.sprite = sprites[Random.Range(0, sprites.Length)];
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
