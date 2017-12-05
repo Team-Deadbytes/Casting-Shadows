fb15b2444af608f34ad7299e9473da47a01768f4
Hallur A. Atlason <halluratlason@gmail.com>
2017-12-05 18:49:45 +0000

    Door sounds (#27)
    * Implemented so doors make sounds when being opened/closed
    
    * Removed whitespace from file names

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -7,9 +7,18 @@ public class DoorController : MonoBehaviour
 	private const string CLOSE_MESSAGE = "Press E to close door";
 	private const string OPEN_MESSAGE  = "Press E to open door";
 
+	public AudioClip openSound;
+	public AudioClip closeSound;
+
+	private AudioSource audioSource;
 	private string proximityMessage = OPEN_MESSAGE;
 	private bool showProximityMessage;
 	private bool isOpen;
+
+	public void Start()
+	{
+		audioSource = GetComponent<AudioSource>();
+	}
 
 	public void OnTriggerEnter2D(Collider2D other)
 	{
@@ -35,6 +44,10 @@ public class DoorController : MonoBehaviour
 		float rotationAngle = isOpen ? -90.0f : 90.0f;
 		transform.Rotate(0.0f, 0.0f, rotationAngle);
 		isOpen = !isOpen;
+
+		audioSource.clip = isOpen ? openSound : closeSound;
+		audioSource.Play();
+
 		proximityMessage = !isOpen ? OPEN_MESSAGE : CLOSE_MESSAGE;
 	}
 
