4874387104755ac248cc4178ab336348160daee9
dadisigursveinn <dadi.sigursveinn@gmail.com>
2017-12-05 22:21:59 +0000

    Fixed indetation in TableLightController

--- Assets/Scripts/TableLightController.cs
+++ Assets/Scripts/TableLightController.cs
@@ -2,23 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TableLightController : MonoBehaviour {
+public class TableLightController : MonoBehaviour
+{
     private SpriteRenderer renderer;
     public Sprite[] sprites;
 
     [SerializeField]
     bool state;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         renderer = gameObject.GetComponent<SpriteRenderer>();
         if (state)
-        {
             renderer.sprite = sprites[state ? 1 : 0];
-        }
     }
-	
-	// Update is called once per frame
-	void Update () {
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
 }
