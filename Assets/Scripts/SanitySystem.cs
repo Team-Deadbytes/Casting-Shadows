a2796120e4600c8ef1dfa11a125e007cff33d865
dadisigursveinn <dadi.sigursveinn@gmail.com>
2017-12-06 02:19:50 +0000

    Fixed indentation

--- Assets/Scripts/SanitySystem.cs
+++ Assets/Scripts/SanitySystem.cs
@@ -2,32 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SanitySystem : MonoBehaviour {
+public class SanitySystem : MonoBehaviour
+{
 
     public float sanity;
     private bool inSafeZone;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         sanity = 20.0f;
         inSafeZone = true;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (!inSafeZone && sanity > 0.0f)
         {
             sanity -= 1.5f * Time.deltaTime;
         }
-        else if(inSafeZone && sanity < 20.0f)
+        else if (inSafeZone && sanity < 20.0f)
         {
             sanity += 2.5f * Time.deltaTime;
         }
-	}
+    }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if(collision.tag == "Light")
+        if (collision.tag == "Light")
         {
             inSafeZone = false;
         }
@@ -35,7 +38,7 @@ public class SanitySystem : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Light")
+        if (collision.tag == "Light")
         {
             inSafeZone = true;
         }
