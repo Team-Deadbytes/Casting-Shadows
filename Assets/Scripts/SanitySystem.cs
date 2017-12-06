5f133a200c0626e148c7db629d0bc3506af2657a
Sölvi Sig <solvisig@gmail.com>
2017-12-06 02:20:19 +0000

    Initial sanity system. (#30)

---
+++ Assets/Scripts/SanitySystem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SanitySystem : MonoBehaviour
+{
+
+    public float sanity;
+    private bool inSafeZone;
+
+    // Use this for initialization
+    void Start()
+    {
+        sanity = 20.0f;
+        inSafeZone = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!inSafeZone && sanity > 0.0f)
+        {
+            sanity -= 1.5f * Time.deltaTime;
+        }
+        else if (inSafeZone && sanity < 20.0f)
+        {
+            sanity += 2.5f * Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Light")
+        {
+            inSafeZone = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Light")
+        {
+            inSafeZone = true;
+        }
+    }
+}
