52e66d29d7bf0d2ec4227527e9166e15e6a82888
Sölvi Sig <solvisig@gmail.com>
2017-12-06 22:36:13 +0000

    Sounds and prefabs to trigger sounds

---
+++ Assets/PlayAudio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayAudio : MonoBehaviour {
+    private AudioSource audioSource;
+
+    private void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player" && other.isTrigger == false)
+        {
+            audioSource = GetComponent<AudioSource>();
+            if(!audioSource.isPlaying)
+                audioSource.Play();
+        }
+}
+}
