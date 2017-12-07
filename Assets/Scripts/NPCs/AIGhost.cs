14fe9df5020c15c8cfe1671c9a3203f09bbb7449
Njall Hilmar Hilmarsson <njall13@ru.is>
2017-12-07 20:35:38 +0000

    Made the ai able to trailback and gave it ability to know its last position when it fails to catch player

--- Assets/Scripts/NPCs/AIGhost.cs
+++ Assets/Scripts/NPCs/AIGhost.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class AIGhost : MonoBehaviour
 {
     public Transform[] target;
-    public float speed;
-    public int currTarget;
+    public float patrolSpeed;
+    public float chaseSpeed;
+    public int currTarget, lastTarget;
     private Collider2D prevCollider;
     private GameObject playerObject;
     private GameObject doorObject;
     public Canvas deathCanvas;
+
+    [SerializeField]
+    public float realspeed;
+    [SerializeField]
+    public float speedCheck;
+    Vector3 lastPos;
+    int trailback;
+    bool waiting;
 
     private SanitySystem sanitySystem;
     private DoorController doorController;
@@ -18,7 +27,11 @@ public class AIGhost : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        trailback = 0;
+        lastPos = this.gameObject.transform.position;
+        speedCheck = 0.0f;
         currTarget = 1;
+        lastTarget = 1;
         playerObject = GameObject.Find("Player");
         if(playerObject != null)
             sanitySystem = playerObject.GetComponentInChildren<SanitySystem>();
@@ -29,7 +42,11 @@ public class AIGhost : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float step = speed * Time.deltaTime;
+        float step;
+        if (currTarget == 0)
+            step = chaseSpeed * Time.deltaTime;
+        else
+            step = patrolSpeed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, target[currTarget].position, step);
         if(currTarget == 0 && playerObject != null)
             sanitySystem.sanity -= 3.0f * Time.deltaTime;
@@ -37,6 +54,33 @@ public class AIGhost : MonoBehaviour
             sanitySystem.isSeen(true);
         else
             sanitySystem.isSeen(false);
+
+        if (!waiting)
+        {
+            Vector3 diffpos = lastPos - this.gameObject.transform.position;
+            realspeed += diffpos.magnitude;
+            speedCheck += Time.deltaTime;
+            if (speedCheck > 6.0f)
+            {
+                if (realspeed < 4.5f)
+                {
+                    trailback = 2;
+                    currTarget -= 1;
+                    if (currTarget <= 0)
+                    {
+                        int num = Random.Range(1, target.Length);
+                        Transform waypoint = target[num];
+                        this.gameObject.transform.position = waypoint.position;
+                        currTarget = num + 1;
+                        if (currTarget == target.Length)
+                            currTarget = 1;
+                    }
+                }
+                speedCheck = 0.0f;
+                realspeed = 0.0f;
+            }
+            lastPos = this.gameObject.transform.position;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -45,19 +89,40 @@ public class AIGhost : MonoBehaviour
         {
             prevCollider = other;
             if (other.gameObject.tag == "Player")
+            {
+                lastTarget = currTarget;
                 currTarget = 0;
+            }
             else if (other.gameObject.tag == "Waypoint" && other.gameObject == target[currTarget].gameObject)
             {
-                StartCoroutine(Wait());
+                if (trailback > 0)
+                {
+                    if(trailback == 1)
+                    {
+                        lastTarget = currTarget;
+                        currTarget += 2;
+                        if (currTarget == target.Length)
+                            currTarget = 1;
+                    } else
+                    {
+                        lastTarget = currTarget;
+                        currTarget--;
+                        if (currTarget == 0)
+                            currTarget = target.Length - 1;
+                    }
+                    trailback--;
+                } else
+                    StartCoroutine(Wait());
             }
             else if (other.gameObject.tag == "Light")
             {
                 Light otherlit = other.gameObject.GetComponent<Light>();
                 if (otherlit.enabled)
                 {
+                    Debug.Log("Light " + other.gameObject.tag);
                     CeilingLight lightsystem = other.GetComponentInParent<CeilingLight>();
                     lightsystem.MonsterProwing(true);
-                    
+
                     currTarget = 1;
                 }
             }
@@ -65,7 +130,9 @@ public class AIGhost : MonoBehaviour
                 return;
             else if (other.gameObject.tag != "Objects" && other.gameObject.tag != "Waypoint")
             {
-                currTarget = 1;
+                Debug.Log("Unknown " + other.gameObject.tag);
+                currTarget -= 1;
+                trailback = 2;
             }
         }
     }
@@ -75,7 +142,7 @@ public class AIGhost : MonoBehaviour
         if (collision.gameObject.tag == "Player")
         {
             if (currTarget < target.Length - 1)
-                currTarget = 1;
+                currTarget = lastTarget;
         }
         else if (collision.gameObject.tag == "Light" && collision.isActiveAndEnabled)
         {
@@ -104,7 +171,10 @@ public class AIGhost : MonoBehaviour
     IEnumerator Wait()
     {
         //print(Time.time);
+        waiting = true;
         yield return new WaitForSecondsRealtime(Random.Range(0, 4));
+        waiting = false;
+        lastTarget = currTarget;
         if (currTarget < target.Length - 1)
             currTarget += 1;
         else
