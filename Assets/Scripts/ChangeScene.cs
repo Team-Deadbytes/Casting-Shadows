4e3df332ec2115aef39d5b2c5a24b47efb327e6a
Sölvi Sig <solvisig@gmail.com>
2017-12-07 15:47:43 +0000

    win / lose

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -6,7 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-
     public void ChangeToScene(string SceneToChange)
     {
         SceneManager.LoadScene(SceneToChange);
