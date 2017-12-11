e405b1d42777f8a01e9fd0a5a492ffe8397a64c5
Njall Hilmar Hilmarsson <njall13@ru.is>
2017-12-11 13:40:49 +0000

    initial example of astar

---
+++ Assets/Scripts/NPCs/AIGhost2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIGhost2 : MonoBehaviour {
+    enum Style { AGGRESIVE, PROWLER, SABOTEUR };
+    enum Pathing { PATROLLER, WONDERER };
+
+    [SerializeField]
+    Pathing pathingPreference;
+    [SerializeField]
+    Style strategyPreference;
+
+    private GameObject player;
+
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
