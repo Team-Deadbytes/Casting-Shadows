c9032dfffb5bf34f896f428a1666d1b7fee72572
Hallur A. Atlason <halluratlason@gmail.com>
2017-12-12 16:51:58 +0000

    Made the action of opening/searching the drawer timed

--- Assets/Scripts/Enviroment/DrawerController.cs
+++ Assets/Scripts/Enviroment/DrawerController.cs
@@ -1,31 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DrawerController : MonoBehaviour
 {
-    private const string SEARCH_MESSAGE  = "Press E to search the drawers";
-    private const string CLOSE_MESSAGE = "Press E to close the drawers";
-
     private SpriteRenderer sr;
-    public Sprite closedSprite;
-    public Sprite open1Sprite;
-    // public Sprite open2Sprite;
-
-    public bool open1;
-    // public bool open2;
 
     public int LightBulbCount;
 
-    private ProximityMessage proximityMessage;
     private Inventory playersInventory;
+
+    private float totalActionTime = 1.5f;
+    private bool interacting;
+    private float actionProgress;
+    Vector3 playerPosition;
+
+    private Animator animator;
+    private GameObject progressBar;
+    private Image progressBarImg;
 
     void Start ()
     {
-        sr = GetComponent<SpriteRenderer>();
-        proximityMessage = GetComponent<ProximityMessage>();
-        SetSprite();
-        SetProximityMessage();
+        progressBar = GameObject.Find("/Canvas/ChangeLightProgressBar");
+        animator = GetComponent<Animator>();
+        progressBarImg = progressBar.GetComponent<Image>();
     }
 
     public void OnTriggerEnter2D(Collider2D other)
@@ -37,51 +36,76 @@ public class DrawerController : MonoBehaviour
     public void OnTriggerStay2D(Collider2D other)
     {
         if (other.tag == "Player" && !other.isTrigger)
-            if (Input.GetKeyDown(KeyCode.E))
-                ToggleDrawer();
-    }
-
-    public void ToggleDrawer()
-    {
-        open1 = !open1;
-        if (open1)
         {
-            if (LightBulbCount > 0)
+            if (Input.GetKeyDown(KeyCode.E))
             {
-                // TODO: Display message "You found X light bulbs" etc.
-                Debug.Log("You found " + LightBulbCount + " light bulbs");
-                for (int i = 0; i < LightBulbCount; i++)
-                    playersInventory.AddLightBulb(new LightBulb());
-                LightBulbCount = 0;
+                playerPosition = other.transform.position;
+                StartAction();
             }
-            else
+
+            if (interacting)
             {
-                // TODO: Display message "There is nothing in these drawers" etc.
-                Debug.Log("There is nothing in these drawers");
+                if (Input.GetKeyUp(KeyCode.E))
+                {
+                    StopAction();
+                    animator.SetTrigger("StopOpening");
+                }
+                
+                if (playerPosition == other.transform.position)
+                    ProgressAction();
+                else
+                {
+                    StopAction();
+                    animator.SetTrigger("StopOpening");
+                }
             }
         }
-        SetSprite();
-        SetProximityMessage();
     }
 
-    private void SetSprite()
+    private void StartAction()
     {
-        // If both open1 and open2 are true, then open1Sprite is rendered.
-        // Perhaps add a sprite with both drawers open?
-        if (open1)
-            sr.sprite = open1Sprite;
-        // else if (open2)
-        //     sr.sprite = open2Sprite;
-        else
-            sr.sprite = closedSprite;
+        interacting = true;
+        progressBarImg.enabled = true;
+        animator.SetTrigger("Open");
     }
 
-    private void SetProximityMessage()
+    private void ProgressAction()
     {
-        // if (!open1 && !open2)
-        if (!open1)
-            proximityMessage.Message = SEARCH_MESSAGE;
+        actionProgress += Time.deltaTime;
+
+        progressBarImg.fillAmount = actionProgress / totalActionTime;
+
+        if (actionProgress >= totalActionTime)
+            FinishAction();
+    }
+
+    private void FinishAction()
+    {
+        StopAction();
+        SearchDrawers();
+    }
+
+    private void StopAction()
+    {
+        interacting = false;
+        actionProgress = 0;
+        progressBarImg.enabled = false;
+    }
+
+    private void SearchDrawers()
+    {
+        if (LightBulbCount > 0)
+        {
+            // TODO: Display message "You found X light bulbs" etc.
+            Debug.Log("You found " + LightBulbCount + " light bulbs");
+            for (int i = 0; i < LightBulbCount; i++)
+                playersInventory.AddLightBulb(new LightBulb());
+            LightBulbCount = 0;
+        }
         else
-            proximityMessage.Message = CLOSE_MESSAGE;
+        {
+            // TODO: Display message "There is nothing in these drawers" etc.
+            Debug.Log("There is nothing in these drawers");
+        }
     }
 }
