b53372b4df1f4731c442c628cb692431a5e0d251
Sölvi Sig <solvisig@gmail.com>
2017-12-13 15:00:23 +0000

    esc will bring pause menu with quit game and resume game buttons

--- Assets/Scripts/GameMgr.cs
+++ Assets/Scripts/GameMgr.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameMgr : MonoBehaviour
 {
+    public GameObject PauseMenu;
+    private bool isPaused;
+
     void Awake()
     {
         DontDestroyOnLoad(transform.gameObject);
@@ -11,11 +14,35 @@ public class GameMgr : MonoBehaviour
 
     void Start()
     {
-
+        isPaused = false;
     }
 
     void Update()
     {
+        if (Input.GetKeyDown("escape") && isPaused == false)
+        {
+            PauseMenu.SetActive(true);
+            Time.timeScale = 0.0f;
+            isPaused = true;
+        }
+        else if (Input.GetKeyDown("escape") && isPaused == true)
+        {
+            PauseMenu.SetActive(false);
+            Time.timeScale = 1.0f;
+            isPaused = false;
+        }
+        if (isPaused == false)
+            PauseMenu.SetActive(false);
+    }
 
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1.0f;
+        isPaused = false;
     }
 }
