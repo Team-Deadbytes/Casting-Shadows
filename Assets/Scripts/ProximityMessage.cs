f79fbae3232e924628ecde15e28cc40ec69d843e
Sölvi Sig <solvisig@gmail.com>
2017-12-14 13:47:36 +0000

    ProximityMessage modification

--- Assets/Scripts/ProximityMessage.cs
+++ Assets/Scripts/ProximityMessage.cs
@@ -5,19 +5,44 @@ using UnityEngine;
 public class ProximityMessage : MonoBehaviour
 {
 	public string Message;
-	private bool show;
+    public bool TypeEffect;
+    private bool show;
+    public int height;
+    private string tmpMessage;
+    public int FontSize;
 
-	public void OnTriggerEnter2D(Collider2D other)
+    private void Start()
+    {
+        if (height == 0)
+            height = 100;
+        if (FontSize == 0)
+            FontSize = 20;
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.tag == "Player" && !other.isTrigger)
+        { 
 			show = true;
-	}
+            if (TypeEffect)
+            {
+                tmpMessage = Message;
+                Message = "";
+                StartCoroutine(TypeF());
+            }
+        }
+    }
 
 	public void OnTriggerExit2D(Collider2D other)
 	{
 		if (other.tag == "Player" && !other.isTrigger)
+        { 
 			show = false;
-	}
+            StopAllCoroutines();
+            if(TypeEffect)
+                Message = tmpMessage;
+        }
+    }
 
 	private void OnGUI()
 	{
@@ -25,14 +50,30 @@ public class ProximityMessage : MonoBehaviour
 		{
 			GUIStyle labelStyle = GUI.skin.GetStyle("Label");
 			labelStyle.alignment = TextAnchor.UpperCenter;
+            labelStyle.fontSize = FontSize;
 
-			float labelWidth = 300f;
-			float labelHeight = 50f;
+			float labelWidth = 600f;
+			float labelHeight = 80f;
 			float labelX = (Screen.width - labelWidth) / 2;
-			float labelY = Screen.height - 100;
+			float labelY = Screen.height - height;
 			Rect labelRect = new Rect(labelX, labelY, labelWidth, labelHeight);
 
 			GUI.Label(labelRect, Message, labelStyle);
 		}
 	}
-}
+
+    IEnumerator TypeF()
+    {
+        if (show)
+        {
+            foreach (char letter in tmpMessage.ToCharArray())
+            {
+                Message += letter;
+                //if (typeSound1 && typeSound2)
+                //  SoundManager.instance.RandomizeSfx(typeSound1, typeSound2);
+                yield return 0;
+                yield return new WaitForSeconds(Random.Range(0.05f,0.15f));
+            }
+        }
+    }
+ }
