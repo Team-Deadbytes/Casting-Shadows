e2aecc807ceb4eb83bd723a5618d4a3ccaa09eda
Njall Hilmar Hilmarsson <njall13@ru.is>
2017-12-16 18:00:36 +0000

    monsters no longer base kill range based on completion of path, instead based on circle collider

---
+++ Assets/Scripts/NPCs/AIGhost2Kill.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIGhost2Kill : MonoBehaviour
+{
+    AIGhost2 parObject;
+
+    // Use this for initialization
+    void Start ()
+    {
+        parObject = GetComponentInParent<AIGhost2>();
+    }
+	
+    // Update is called once per frame
+    void Update ()
+    {	
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Player top light")
+            parObject.onTriggerKillEnter(other);
+    }
+}
