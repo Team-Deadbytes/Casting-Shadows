d6ca3f58e5148fbea64da3a08345d5b80801a3dc
Sölvi Sig <solvisig@gmail.com>
2017-12-16 15:24:05 +0000

    Credit list prefab created.

---
+++ Assets/CreditScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditScript : MonoBehaviour {
+
+    private Animator anim;
+
+    // Use this for initialization
+    void Start() {
+        anim = GetComponent<Animator>();
+        anim.StopPlayback();
+        anim.SetBool("playtrigger", false);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void StartCreditList()
+    {
+        this.gameObject.SetActive(true);
+        anim.SetBool("playtrigger", true);
+        anim.Play("Credidt");
+    }
+
+    public void StopAnimation()
+    {
+        anim.SetBool("playtrigger", false);
+    }
+}
