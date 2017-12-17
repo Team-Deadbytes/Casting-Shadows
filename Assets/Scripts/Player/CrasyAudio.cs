160893909c40dfd190ee538fc94deb663486b8f0
Daði Sigursveinn Harðarson <dadi.sigursveinn@gmail.com>
2017-12-17 21:37:01 +0000

    Just lose it (#161)
    * removed compression from images
    
    * Added End song
    
    * Added blood arrows
    
    * created blood arrows prefabs
    
    * Added arrows to level 1
    
    * level1 back to master ok
    
    * Blood arrows back in lelvel1
    
    * created bood arrow prefab for level 1
    
    * Level1 back from master
    
    * added blood arrows prefab to level 1
    
    * player hears sounds when he looses sanity
    
    * level1 reveted to master
    
    * amplified audio

---
+++ Assets/Scripts/Player/CrasyAudio.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrasyAudio : MonoBehaviour
+{
+
+    AudioSource audioSource;
+    SanitySystem sanity;
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        sanity = GameObject.Find("Units/Player/Player top light").GetComponent<SanitySystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sanity.sanity <= 40)
+            audioSource.volume = 1;
+        else
+            audioSource.volume = 0;
+    }
+}
